dea61a898382ed8db26da9f7c8fc546b111016ef
NickRisinger <boozy044@gmail.com>
2024-06-15 15:54:14 +0300

    ci(deploy): update deploy action

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: 'tasy',
+	base: process.env.STAGE ? 'tasy' : '',
 	css: {
 		preprocessorOptions: {
 			scss: {
